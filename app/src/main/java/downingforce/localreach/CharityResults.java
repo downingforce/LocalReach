d56d7c4e28e6d92206756ad4b727d5ecac8de7c1
Jason Downing <downingforce@gmail.com>
2016-11-20 23:32:22 -0800

    add CharityResults activity charityTestArray with activity array adapter

--- app/src/main/java/downingforce/localreach/CharityResults.java
+++ app/src/main/java/downingforce/localreach/CharityResults.java
@@ -3,16 +3,23 @@ package downingforce.localreach;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class CharityResults extends AppCompatActivity {
     private TextView mResultsLabel;
+    private String[] charitiesTestArray = new String[] {"Red Cross", "Habitat for Humanity", "Charity Water", "United Way", "PBS"};
+    private ListView mCharityListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charity_results);
 
+        mCharityListView = (ListView) findViewById(R.id.charityList);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, charitiesTestArray);
+        mCharityListView.setAdapter(adapter);
         mResultsLabel = (TextView) findViewById(R.id.resultsLabel);
 
         Intent intent = getIntent();
