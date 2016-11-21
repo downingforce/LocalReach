7a1e3edb164bf1df6c94d7c5cabe181710cb677c
Jason Downing <downingforce@gmail.com>
2016-11-20 22:55:11 -0800

    add EditText field for zipe code confirmation to CharityResults activity

--- app/src/main/java/downingforce/localreach/CharityResults.java
+++ app/src/main/java/downingforce/localreach/CharityResults.java
@@ -1,13 +1,22 @@
 package downingforce.localreach;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CharityResults extends AppCompatActivity {
+    private TextView mResultsLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charity_results);
+
+        mResultsLabel = (TextView) findViewById(R.id.resultsLabel);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mResultsLabel.setText("Charities for: " + location);
     }
 }
--- app/src/main/java/downingforce/localreach/MainActivity.java
+++ app/src/main/java/downingforce/localreach/MainActivity.java
@@ -5,20 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button mSearchCharitiesButton;
+    private EditText mZipCodeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mZipCodeEditText = (EditText) findViewById(R.id.zipCode);
         mSearchCharitiesButton = (Button) findViewById(R.id.seachCharitiesButton);
         mSearchCharitiesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String location = mZipCodeEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, CharityResults.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
