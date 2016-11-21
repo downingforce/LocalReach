417f1d33997716a147a888d2f28a74fbd78888e7
Jason Downing <downingforce@gmail.com>
2016-11-20 22:05:53 -0800

    create CharityResults activity along with the corresponding SearchCharities intent on MainActivity

---
+++ app/src/main/java/downingforce/localreach/CharityResults.java
@@ -0,0 +1,13 @@
+package downingforce.localreach;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharityResults extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charity_results);
+    }
+}
--- app/src/main/java/downingforce/localreach/MainActivity.java
+++ app/src/main/java/downingforce/localreach/MainActivity.java
@@ -1,13 +1,26 @@
 package downingforce.localreach;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSearchCharitiesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchCharitiesButton = (Button) findViewById(R.id.seachCharitiesButton);
+        mSearchCharitiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CharityResults.class);
+                startActivity(intent);
+            }
+        });
     }
 }
