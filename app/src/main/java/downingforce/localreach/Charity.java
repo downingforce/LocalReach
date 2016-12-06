d754deeea2209576602cc768fb938784e3aa0986
Jason Downing <downingforce@gmail.com>
2016-12-06 11:37:41 -0800

    parse API response in GuideStarService class and create Charity class

---
+++ app/src/main/java/downingforce/localreach/Charity.java
@@ -0,0 +1,107 @@
+package downingforce.localreach;
+
+public class Charity {
+    private String mEin;
+    private String mName;
+    private String mMission;
+    private String mCity;
+    private String mState;
+    private String mZip;
+    private String mPublicReport;
+    private String mWebsite;
+    private String mLogo;
+    private String mNteecode;
+
+    public Charity(String mEin, String mName, String mMission, String mCity, String mState, String mZip, String mPublicReport, String mWebsite, String mLogo, String mNteecode) {
+        this.mEin = mEin;
+        this.mName = mName;
+        this.mMission = mMission;
+        this.mCity = mCity;
+        this.mState = mState;
+        this.mZip = mZip;
+        this.mPublicReport = mPublicReport;
+        this.mWebsite = mWebsite;
+        this.mLogo = mLogo;
+        this.mNteecode = mNteecode;
+    }
+
+    public String getmEin() {
+        return mEin;
+    }
+
+    public void setmEin(String mEin) {
+        this.mEin = mEin;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmMission() {
+        return mMission;
+    }
+
+    public void setmMission(String mMission) {
+        this.mMission = mMission;
+    }
+
+    public String getmCity() {
+        return mCity;
+    }
+
+    public void setmCity(String mCity) {
+        this.mCity = mCity;
+    }
+
+    public String getmState() {
+        return mState;
+    }
+
+    public void setmState(String mState) {
+        this.mState = mState;
+    }
+
+    public String getmZip() {
+        return mZip;
+    }
+
+    public void setmZip(String mZip) {
+        this.mZip = mZip;
+    }
+
+    public String getmPublicReport() {
+        return mPublicReport;
+    }
+
+    public void setmPublicReport(String mPublicReport) {
+        this.mPublicReport = mPublicReport;
+    }
+
+    public String getmWebsite() {
+        return mWebsite;
+    }
+
+    public void setmWebsite(String mWebsite) {
+        this.mWebsite = mWebsite;
+    }
+
+    public String getmLogo() {
+        return mLogo;
+    }
+
+    public void setmLogo(String mLogo) {
+        this.mLogo = mLogo;
+    }
+
+    public String getmNteecode() {
+        return mNteecode;
+    }
+
+    public void setmNteecode(String mNteecode) {
+        this.mNteecode = mNteecode;
+    }
+}
--- app/src/main/java/downingforce/localreach/GuideStarService.java
+++ app/src/main/java/downingforce/localreach/GuideStarService.java
@@ -2,10 +2,19 @@ package downingforce.localreach;
 
 
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class GuideStarService {
 
@@ -26,4 +35,40 @@ public class GuideStarService {
         Call call = client.newCall(request);
         call.enqueue(callback);
     }
+
+    public ArrayList<Charity> processResults(Response response) {
+        ArrayList<Charity> charities = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONObject guideStarJSON = new JSONObject(jsonData);
+                JSONArray charitiesJSON = guideStarJSON.getJSONArray("hits");
+                for (int i = 0; i < charitiesJSON.length(); i++) {
+                    JSONObject charityJSON = charitiesJSON.getJSONObject(i);
+                    String ein = charityJSON.getString("ein");
+                    String name = charityJSON.getString("organization_name");
+                    String mission = charityJSON.getString("mission");
+                    String city = charityJSON.getString("city");
+                    String state = charityJSON.getString("state");
+                    String zip = charityJSON.getString("zip");
+                    String report = charityJSON.getString("public_report");
+                    String website = charityJSON.getString("website");
+                    String logo = charityJSON.getString("logo_url");
+                    String nteecode = charityJSON.getString("nteecode");
+                    if (logo.equals("null")) {
+                        logo = "https://unsplash.it/200/300/?random";
+                    }
+                    Charity charity = new Charity(ein, name, mission, city, state, zip, report, website, logo, nteecode);
+                    charities.add(charity);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return charities;
+    }
+
 }
