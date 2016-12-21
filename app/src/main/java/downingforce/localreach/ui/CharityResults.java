f18a9a66930e7ca0e5a694e1272712efa304dcf6
Jason Downing <downingforce@gmail.com>
2016-12-20 19:46:36 -0800

    refactor MainActivity and create FirebaseCharityViewHolder to add search widget to comb through previous Firebase searches

--- app/src/main/java/downingforce/localreach/ui/CharityResults.java
+++ app/src/main/java/downingforce/localreach/ui/CharityResults.java
@@ -3,10 +3,15 @@ package downingforce.localreach.ui;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import downingforce.localreach.Constants;
 import downingforce.localreach.R;
@@ -25,8 +30,9 @@ import okhttp3.Callback;
 
 public class CharityResults extends AppCompatActivity {
 
-//    private SharedPreferences mSharedPreferences;
-//    private String mRecentZip;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentZip;
 
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
     private CharityListAdapter mAdapter;
@@ -44,11 +50,41 @@ public class CharityResults extends AppCompatActivity {
 
         getCharities(location);
 
-//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//        mRecentZip = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
-//        if (mRecentZip != null) {
-//            getCharities(mRecentZip);
-//        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getCharities(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     private void getCharities(String location) {
@@ -78,5 +114,8 @@ public class CharityResults extends AppCompatActivity {
                 });
             }
         });
+    }
+    private void addToSharedPreferences(String location) {
+        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
     }
 }
--- app/src/main/java/downingforce/localreach/ui/MainActivity.java
+++ app/src/main/java/downingforce/localreach/ui/MainActivity.java
@@ -24,47 +24,17 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-//    private SharedPreferences mSharedPreferences;
-//    private SharedPreferences.Editor mEditor;
-    private DatabaseReference mSearchedLocationReference;
-    private ValueEventListener mSearchedLocationReferenceListener;
-
     @Bind(R.id.seachCharitiesButton) Button mSearchCharitiesButton;
     @Bind(R.id.zipCode) EditText mZipCodeEditText;
     @Bind(R.id.myCharitiesButton) Button mMyCharitiesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mSearchedLocationReference = FirebaseDatabase
-                .getInstance()
-                .getReference()
-                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
-
-        mSearchedLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
-
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
-                    String location = locationSnapshot.getValue().toString();
-                    Log.d("Locations updated", "location: " + location);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-
-        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//        mEditor = mSharedPreferences.edit();
-
-        //click listener on button to pass user entered zipcode to CharityResults activity
         mSearchCharitiesButton.setOnClickListener(this);
         mMyCharitiesButton.setOnClickListener(this);
     }
@@ -72,13 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v == mSearchCharitiesButton) {
-            String location = mZipCodeEditText.getText().toString();
-            saveLocationToFirebase(location);
-//            if(!(location).equals("")) {
-//                addToSharedPreferences(location);
-//            }
             Intent intent = new Intent(MainActivity.this, CharityResults.class);
-            intent.putExtra("location", location);
             startActivity(intent);
         }
         if (v == mMyCharitiesButton) {
@@ -86,20 +50,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         }
     }
-
-    public void saveLocationToFirebase(String location) {
-        mSearchedLocationReference.push().setValue(location);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
-    }
-
-
-//    private void addToSharedPreferences(String location) {
-//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-//    }
 }
 
