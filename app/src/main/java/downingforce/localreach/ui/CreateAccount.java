1ec76f7f91b3b7cc8ea9f15a49029edb0550eacb
Jason Downing <downingforce@gmail.com>
2016-12-20 20:05:18 -0800

    create account creation and login layouts

---
+++ app/src/main/java/downingforce/localreach/ui/CreateAccount.java
@@ -0,0 +1,15 @@
+package downingforce.localreach.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import downingforce.localreach.R;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
